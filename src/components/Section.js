5630314d58fcaa98b2fb7a3460cceb184065e743
Htut Oo Aung <aung.hoa@gmail.com>
2022-01-30 20:29:16 +0700

    ADD: Section for the TESLA

---
+++ src/components/Section.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+function Section({title, description, leftBtnText, rightBtnText, backgroundImg}) {
+  return <Wrap bgImage={backgroundImg}>
+      <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+            <LeftButton>
+                {leftBtnText}
+            </LeftButton>
+            {rightBtnText && 
+                <RightButton>
+                    {rightBtnText}
+                </RightButton>
+            } 
+        </ButtonGroup>
+      <DownArrow src="/images/down-arrow.svg"/>
+      </Buttons>
+      
+  </Wrap>;
+}
+
+export default Section;
+
+const Wrap = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center; //horizontal
+    background-image: ${props => `url('/images/${props.bgImage}')`}
+`
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    color: #fff;
+    height: 40px;
+    width: 256px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 8px;
+`
+const RightButton = styled(LeftButton)`
+    background: white;
+    opacity: 0.65;
+    color: black;
+`
+
+const DownArrow = styled.img`
+    height: 40px;
+    animation: animateDown infinite 1.5s;
+    overflow-x: hidden;
+    margin-bottom: 5px;
+`
+
+const Buttons = styled.div`
+
+`
