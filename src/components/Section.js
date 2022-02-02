b3362fe6bd4d9b55d8b4c2a9ca5591df6b4dffe1
Htut Oo Aung <aung.hoa@gmail.com>
2022-02-02 18:36:01 +0700

    ADD:redux for cars

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
+import Fade from 'react-reveal/Fade';
 
 
 function Section({title, description, leftBtnText, rightBtnText, backgroundImg}) {
   return <Wrap bgImage={backgroundImg}>
-      <ItemText>
-          <h1>{title}</h1>
-          <p>{description}</p>
-      </ItemText>
-      <Buttons>
-        <ButtonGroup>
-            <LeftButton>
-                {leftBtnText}
-            </LeftButton>
-            {rightBtnText && 
-                <RightButton>
-                    {rightBtnText}
-                </RightButton>
-            } 
-        </ButtonGroup>
-      <DownArrow src="/images/down-arrow.svg"/>
-      </Buttons>
+    <Fade bottom>
+        <ItemText>
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </ItemText>
+    </Fade>
+
+        <Buttons>
+            <Fade bottom >
+                <ButtonGroup>
+                    <LeftButton>
+                        {leftBtnText}
+                    </LeftButton>
+                    {rightBtnText && 
+                        <RightButton>
+                            {rightBtnText}
+                        </RightButton>
+                    } 
+                </ButtonGroup>
+            </Fade>
+            
+            <DownArrow src="/images/down-arrow.svg"/>
+        </Buttons>
       
   </Wrap>;
 }
